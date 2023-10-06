079a29c31adf30c8ba9837be4dfd0b61fa33799b
Umur Deniz Dikmen <117382440+UmurDenizDikmen@users.noreply.github.com>
2023-10-06 19:20:49 +0300

    deleted

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = {
-  eslint: { ignoreDuringBuilds: true },
-  async redirects() {
-    return [
-      {
-        source: "/auth",
-        destination: "/profile",
-        permanent: true,
-      },
-    ];
-  },
-};
+module.exports = { eslint: { ignoreDuringBuilds: true } };
