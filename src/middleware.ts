bac8835798658d2f7d26ac2a611aabea79bffa7f
Umur Deniz Dikmen <117382440+UmurDenizDikmen@users.noreply.github.com>
2023-10-06 16:28:04 +0300

    changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,26 +3,18 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
-  const isPublicPath = path === "netflix-clone-projects.vercel.app/auth";
+  const isPublicPath = path === "/auth";
   const token = request.cookies.get("next-auth.session-token")?.value || "";
 
   if (isPublicPath && token) {
-    return NextResponse.redirect(
-      new URL("/netflix-clone-projects.vercel.app", request.nextUrl)
-    );
+    return NextResponse.redirect(new URL("/", request.nextUrl));
   }
 
   if (!isPublicPath && !token) {
-    return NextResponse.redirect(
-      new URL("netflix-clone-projects.vercel.app/auth", request.nextUrl)
-    );
+    return NextResponse.redirect(new URL("/auth", request.nextUrl));
   }
 }
 
 export const config = {
-  matcher: [
-    "netflix-clone-projects.vercel.app/",
-    "netflix-clone-projects.vercel.app/profile",
-    "netflix-clone-projects.vercel.app/auth",
-  ],
+  matcher: ["/", "/profile", "/auth"],
 };
